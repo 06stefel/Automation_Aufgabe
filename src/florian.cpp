3072256494c8932fa2e321e005cd09258941cace
06florian <06preflo@rgtfo-me.it>
2023-01-18 09:36:21 +0100

    2

---
+++ src/florian.cpp
@@ -0,0 +1,21 @@
+#include "florian.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+float addieren(float x, float y)
+{
+    return x + y;
+}
+float subtrahieren(float x, float y)
+{
+    return x - y;
+}
+float dividieren(float x, float y)
+{
+    return x / y;
+}
+float multiplizieren(float x, float y)
+{
+    return x * y;
+}
